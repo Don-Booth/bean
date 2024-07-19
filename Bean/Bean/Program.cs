910aae009097261907480fead726442ee8ce0184
Don Booth <Don.Booth@live.com>
2024-07-18 23:36:54 -0300

    Initial Commit

---
+++ Bean/Bean/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
